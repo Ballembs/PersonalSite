a28a8e96699221543d18779038c091d4ba63fe89
BallemBs <bsb.balu@gmail.com>
2014-11-03 13:45:53 -0800

    some changes in HOME View

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :about_mes
 
-  resources :homes
+  #resources :homes
+
+  resources :homes , :path => 'WhatAmI'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
